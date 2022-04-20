bd8598dfa6734d1fdf3630cee12d8c905e15b03f
MMartynamajchrzak <mmartynamajchrzak@wp.pl>
2022-04-20 22:58:09 +0200

    reverse admin changes

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-import environ
-
-
-env = environ.Env()
 
 urlpatterns = [
     path('books/', include('books.urls')),
-    path(f'{env.str("ADMIN_URL")}/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path("docs/schema/", SpectacularAPIView.as_view(), name="docs"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="docs"), name="swagger-ui"),
 ]
